1fe3252d82bc43c639fe98b9fefa616ff542ce25
Kanishkb-27 <viniya2play@gmail.com>
2025-08-25 15:57:54 +0300

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,8 @@
+def intro(name):
+    print("Hello, goodevening, I am ",name)
+user_name=input("Enter your name")
+intro(user_name)
+def age(a):
+    print("My age is ",a)
+user_age=int(input("Enter your age"))
+age(user_age)
